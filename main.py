f2b2d13812be1eebdfa5fb684e027bdc3c70c8a1
Max Pavlukhin <pavlukhinm@gmail.com>
2019-11-22 03:39:30 +0300

    Add main.py that runs aiohttp event loop
    Implement Rules Applying System as microservice

---
+++ main.py
@@ -0,0 +1,12 @@
+import aiohttp
+import asyncio
+
+
+async def main():
+    async with aiohttp.ClientSession() as session:
+        pass
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
